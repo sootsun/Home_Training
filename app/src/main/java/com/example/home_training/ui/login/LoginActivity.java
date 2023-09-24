2da41ed10e5a396787e80866370efca7dec9410d
sootsun <87323981+sootsun@users.noreply.github.com>
2023-09-24 12:07:43 +0900

    9.24

---
+++ app/src/main/java/com/example/home_training/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.home_training.ui.login;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.home_training.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
